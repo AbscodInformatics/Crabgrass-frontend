cb6686c8c3e7af501dd1632c622ccf3a97d5ccea
Rajat Verma <rajatv785v@gmail.com>
2023-03-14 17:41:39 +0530

    code

--- src/barcode/BarcodeScanner.js
+++ src/barcode/BarcodeScanner.js
@@ -1,36 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from 'react'
+import useScanDetection from 'use-scan-detection'
 
 const BarcodeScanner = () => {
-  const [barcodeDisplay, setBarcodeDisplay] = useState("No barcode detect");
-  let barCodeScan = "";
+  const [barCodeScan,setBarcodeScan]=useState('No Barcode Scanner');
+  useScanDetection({
+    onComplete: setBarcodeScan,
+    minLength:3,
+    
+  })
+  return (
+    <div>
+      Barcode:{barCodeScan}
+    </div>
+  )
+}
 
-  useEffect(() => {
-    function handleKeyDown(e) {
-      if (e.keyCode === 13 && barCodeScan.length > 3) {
-        handleScan(barCodeScan);
-        return;
-      }
-      if (e.keyCode === 16) {
-        return;
-      }
-      barCodeScan += e.key;
-
-      setTimeout(() => {
-        barCodeScan = "";
-      }, 100);
-    }
-
-    document.addEventListener("keydown", handleKeyDown);
-
-    return function cleanUp() {
-      document.removeEventListener("keydown", handleKeyDown);
-    };
-  });
-
-  const handleScan = (barcodeString) => {
-    setBarcodeDisplay(barcodeString);
-  };
-  return <div>{barcodeDisplay}</div>;
-};
-
-export default BarcodeScanner;
+export default BarcodeScanner
